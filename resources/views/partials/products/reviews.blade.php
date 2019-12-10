5c76bf779ade3228a87442a6567eeb55fc840524
Emanuel <emanuel.guantay92@gmail.com>
2019-12-10 03:00:39 -0300

    Primera version para mostrar los comentarios del producto

---
+++ resources/views/partials/products/reviews.blade.php
@@ -0,0 +1,35 @@
+<div class="align-content-center">
+	<div class="col-12 pt-0 mt-4">
+		<h2 class="text-muted">{{__("Valoraciones")}}</h2><hr/>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			@forelse($product->reviews as $review)
+				<div class="col-md-8 offset-2 listing-block">
+					<div class="media">
+						<!--<img 
+							class="img-rounded" 
+							src="{$review->user->pathAttachment()}}"
+							alt="{$review->user->name}}"
+						/>-->
+						<div>
+							<strong>{{$review->user->name}}</strong>
+						</div>
+						<div class="media-body pl-5">
+							@if($review->comment)
+								<div class="price"><small>{{$review->comment}}</small></div>
+							@endif
+							<div class="stats">
+								{{$review->created_at->format('d/m/Y')}}
+								@include('partials.products.rating',['rating' => $review->rating])
+							</div>
+						</div>
+					</div>
+				</div>
+			@empty
+				<div class="alert alert-dark"><i class="fa fa-info-circle"></i>{{__("Sin valoraciones todavia")}}</div>
+			@endforelse
+		</div>
+	</div>
+	
+</div>
--- resources/views/products/detail.blade.php
+++ resources/views/products/detail.blade.php
@@ -8,8 +8,7 @@
 	<div class="pl-5 pr-5">
 		<div class="row justify-content-center">
 			@include('partials.products.related')
-			
 		</div>
-		
+		@include('partials.products.reviews')
 	</div>
 @endsection
